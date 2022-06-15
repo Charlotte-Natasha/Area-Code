d6084c72fc38333b325ff6b48889b38304e2394e
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-15 22:29:52 +0200

    added header and styled it

--- area/urls.py
+++ area/urls.py
@@ -1,8 +1,6 @@
-from django import views
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('index/', views.index, name='home'),
+    path('', views.index, name='home')
 ]
