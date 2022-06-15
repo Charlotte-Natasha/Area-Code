5fd3a61a614a56a6e5247a9bea2b6d885648fd55
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-15 22:51:40 +0200

    added login and signup details

--- area/urls.py
+++ area/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('home/', views.index, name='home'),
+    path('signup/', views.sign_up, name='signup'),
 ]
