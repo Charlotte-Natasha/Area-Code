fe7b835fd3725d9fbeb2ac592260b6eb16ae8203
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-15 23:01:57 +0200

    added more details to nav

--- area/views.py
+++ area/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    
+
     return render(request, 'area/index.html')
+
+def sign_up(request):
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     email = request.POST.get('email')
+    #     password = request.POST.get('password')
+        
+        
+    #     user = User.objects.create_user(username=username, email=email, password=password)
+
+    #         return redirect('login')
+
+    # else:  
+        return render(request, 'registration/signup.html', {})      
