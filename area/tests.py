66c32a8de16628745c2270e163f24e39bad0e742
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-16 22:37:24 +0200

    added models to admin and tests

--- area/tests.py
+++ area/tests.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import *
+
 
 # Create your tests here.
+
+def test_home_page_status_code(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertEquals(response.status_code, 200)
