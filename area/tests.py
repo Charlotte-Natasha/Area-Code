4149f267a0c5581b036e10e9ac4d9272be915def
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-19 23:16:11 +0200

    added profile and business details

--- area/tests.py
+++ area/tests.py
@@ -11,3 +11,19 @@ def test_home_page_status_code(self):
     url = reverse('home')
     response = self.client.get(url)
     self.assertEquals(response.status_code, 200)
+
+class NeighborhoodTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='hood', password='tashapassword')
+        self.user.save()
+        self.profile = Profile(id=15, user=self.user, )
+        # self.profile.create_profile()
+        self.neighborhood = Neighborhood(name='Test Neighborhood', location='Test Location', population=0,
+                                        description='Test Description', hood_logo='Test Image', admin=self.profile )
+        # self.neighborhood.save()
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+        Neighborhood.objects.all().delete()
+        self.user.delete()    
