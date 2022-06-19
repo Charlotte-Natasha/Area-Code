8e6447c123c39fac05fe9625cd24aa1fb68932ba
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-19 21:43:23 +0200

    implemented login and signup details

--- area/views.py
+++ area/views.py
@@ -12,10 +12,15 @@ def sign_up(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-            
+        image = request.FILES.get('image')
+        
         user = User.objects.create_user(username=username, email=email, password=password)
+        profile = Profile.objects.create(user=user, profile_picture=image)
+        user.save()
+        profile.save()
 
-        return redirect('login')
+        if profile:
+            return redirect('login')
 
     else:  
         return render(request, 'registration/signup.html', {})      
