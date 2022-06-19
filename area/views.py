90d103defe39204caa817059c9fb8e8d9f14f7b5
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-19 22:48:19 +0200

    added leave hood and edited index

--- area/views.py
+++ area/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import *
-from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import *
 
 def index(request):
-
+    
     return render(request, 'area/index.html')
 
 def sign_up(request):
@@ -25,12 +25,14 @@ def sign_up(request):
     else:  
         return render(request, 'registration/signup.html', {})      
 
+@login_required(login_url="/login")
 def profile(request):
     user = request.user
     profile = Profile.objects.get( user = user)
 
     return render(request, 'area/profile.html', {'profile':profile, 'user':user})  
 
+@login_required(login_url="/login")
 def editprofile(request):
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES)
@@ -40,17 +42,20 @@ def editprofile(request):
     else:
         form = ProfileForm()
     return render(request, 'area/editprofile.html', {'form':form})  
-            
+
+@login_required(login_url="/login")            
 def areahood(request):
     all_neighborhoods = Neighborhood.objects.all()
     print(all_neighborhoods)
     return render(request, 'area/areahood.html', {'all_neighborhoods': all_neighborhoods})  
 
+@login_required(login_url="/login")
 def business(request):
     business = Business.objects.all()
 
     return render(request, 'area/business.html', {'business':business})  
 
+@login_required(login_url="/login")
 def new_business(request):
     current_user = request.user
     profile = Profile.objects.get(user = current_user)
@@ -68,6 +73,7 @@ def new_business(request):
         form = NewBusinessForm()
     return render(request, 'area/new-business.html', {"form": form}) 
 
+@login_required(login_url="/login")
 def addhood(request):
     current_user = request.user
     profile = Profile.objects.get(user = current_user)
@@ -83,8 +89,16 @@ def addhood(request):
         form = AddhoodForm()     
     return render(request, 'area/addhood.html', {'form':form})         
 
+@login_required(login_url="/login")
 def join_hood(request, id):
-    neighborhood = Neighborhood.objects.get()
+    neighborhood = Neighborhood.objects.get(id=id)
     request.user.userprofile.neighborhood = neighborhood
     request.user.userprofile.save()
     return redirect('neighborhood')                    
+
+@login_required(login_url="/login")
+def leave_hood(request, id):
+    hood = Neighborhood.objects.get(id=id)
+    request.user.userprofile.neighborhood = None
+    request.user.userprofile.save()
+    return redirect('neighborhood')
